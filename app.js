ec9edb22cc1be363e85c6a0bd01e5aaec5732f43
vshivam05 <sv699291@gmail.com>
2024-04-25 23:50:07 +0530

    login signUp api

---
+++ app.js
@@ -0,0 +1,105 @@
+import express from "express";
+const app = express();
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+import user from "./model/user.js";
+app.use(express.json());
+
+
+
+
+// to get all users ................................
+app.get("/", async (req,res)=>{
+
+    let users;
+try{
+users = await user.find();
+}catch(e){
+console.log(e);
+}
+
+if(!users)
+{
+    res.status(404).json({Message:"No users found"})
+}
+    res.status(200).json({users:users})
+})
+
+
+// api for signup .........................................
+app.post("/signup",async(req,res)=>{
+const {name, email, password} =req.body;
+
+let existingUser;
+try{
+    existingUser = await user.find({email: email});
+}catch(e){
+    res.status(404).json({message:"internal error"});
+}
+
+if(existingUser.length > 0)
+{
+    res.status(400).json({message:"User already Exists"});
+}
+
+const hashPassword = bcrypt.hashSync(password);
+
+const User = new user({
+name:name,
+email:email,
+password: hashPassword,
+
+});
+
+try{
+    await User.save();
+    res.status(201).json({User});
+}catch(e){
+    res.status(500).json({message:"Internal error"})
+}
+
+
+})
+
+
+
+// api for login 
+
+app.post("/login",async (req, res)=>{
+
+    const {email, password} = req.body;
+
+    let existingUser;
+    try{
+existingUser = await user.findOne({email:email});
+    }catch(e){
+        console.log(e);
+    }
+
+    if(!existingUser)
+    {
+        res.status(404).json({message:"User doesn't exists try to signup"})
+    }
+
+    res.status(200).json({message:"Login Successfully"});
+
+
+})
+
+
+
+
+
+mongoose.connect(`mongodb+srv://shivam:shivam123@cluster0.yr7vygl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`).then(()=>{
+    console.log("database connected successfully");
+}).catch((e) => {
+    console.log("error while connect to db");
+  });
+  
+
+
+
+app.listen(5000, ()=>{
+    console.log(`server is listening at PORT 5000`)
+})
